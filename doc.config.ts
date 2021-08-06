5621468df58b81d05ac39fc6c7485fc708031a54
Viktor Dochev <dochevviktor@gmail.com>
2021-08-06 22:37:42 +0200

    Clean up package.json by extracting linter configs to their own files

--- doc.config.ts
+++ doc.config.ts
@@ -1,9 +1,25 @@
 import { replaceInFileSync } from 'replace-in-file';
-import { devDependencies, asciiDocVariableMap, dependencies } from './package.json';
+import { devDependencies, dependencies } from './package.json';
 
 const replace = (): void => {
   const from = [];
   const to = [];
+  const asciiDocVariableMap = {
+    'electron': ':verElectron:',
+    'react': ':verReact:',
+    'redux': ':verRedux:',
+    'typescript': ':verTypescript:',
+    'sass': ':verSass:',
+    '@fortawesome/react-fontawesome': ':verFontAwesome:',
+    'antd': ':verAntDesign:',
+    'eslint': ':verESLint:',
+    'stylelint': ':verStyleLint:',
+    'prettier': ':verPrettier:',
+    'webpack': ':verWebpack:',
+    'rollup': ':verRollup:',
+    '@ffmpeg/ffmpeg': ':verFfmpeg:',
+    'better-sqlite3': ':verSqlite:',
+  };
 
   Object.entries(asciiDocVariableMap).map(([packageDependency, docVariable]) => {
     if (devDependencies[packageDependency]) {
